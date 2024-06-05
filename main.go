684bdb8a71bd8563c0f3de1ca0f1a7401ec6bc32
Ilja Nekrituk <Nekrit99@gmail.com>
2024-06-05 23:18:38 +0300

    home and about handler

--- main.go
+++ main.go
@@ -5,9 +5,27 @@ import (
 	"net/http"
 )
 
-func main() {
+const portNumber = ":8080"
 
-	http.HandlerFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world!")
-	})
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is home page!")
+}
+
+// About is about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValue(5, 7)
+	fmt.Fprintf(w, "This is about page and sum is - %d!", sum)
+}
+
+func addValue(x, v int) int {
+	sum := x + v
+	return sum
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(portNumber, nil)
 }
