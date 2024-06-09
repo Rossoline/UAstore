2020c36ec9b0138b329f02c430cb7a870dd4c099
Ilja <Nekrit99@gmail.com>
2024-06-09 13:35:59 +0300

    divide fucntion

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -18,6 +19,24 @@ func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is about page and sum is - %d!", sum)
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(20.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cant divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f devided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if x <= 0 || y <= 0 {
+		er := errors.New("cant divide by zero")
+		return 0, er
+	}
+	result := x / y
+	return result, nil
+}
+
 func addValue(x, v int) int {
 	sum := x + v
 	return sum
@@ -26,6 +45,6 @@ func addValue(x, v int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-
+	http.HandleFunc("/divide", Divide)
 	_ = http.ListenAndServe(portNumber, nil)
 }
