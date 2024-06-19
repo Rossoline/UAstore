e445a5f71f51b7023b50db0571700364cea83884
Ilja <Nekrit99@gmail.com>
2024-06-19 22:32:30 +0300

    handlers update

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,10 +7,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
-// About is about page handler
+// About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
