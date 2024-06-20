d36052125ae12161115286cfb11e75689cde2dd1
Ilja <Nekrit99@gmail.com>
2024-06-20 21:39:10 +0300

    main with app config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"uastore/pkg/config"
 	"uastore/pkg/handlers"
+	"uastore/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
 	fmt.Print("Server started on port:" + portNumber)
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
