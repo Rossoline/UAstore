157a0a2f4e72d29cd95241a1140739dddf290a1b
Ilja Nekrituk <Nekrit99@gmail.com>
2024-06-26 18:46:34 +0300

    repository

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -18,8 +18,12 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// get the template cache from the app config
-	tc := app.TemplateCache
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
 
 	t, ok := tc[tmpl]
 	if !ok {
