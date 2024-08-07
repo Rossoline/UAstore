bad651af953224251cc8af0d0a183841f319635a
Ilja Nekrituk <Nekrit99@gmail.com>
2024-08-07 22:10:34 +0300

    handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"uastore/pkg/config"
+	"uastore/pkg/models"
 	"uastore/pkg/render"
 )
 
@@ -28,10 +29,14 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello again"
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
