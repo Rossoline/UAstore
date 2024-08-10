159f2166e9a63be8cf6c8b306904878b8d893622
Ilja Nekrituk <Nekrit99@gmail.com>
2024-08-10 23:09:59 +0300

    routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 	"uastore/pkg/config"
 	"uastore/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	//mux := pat.New()
+	//
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
